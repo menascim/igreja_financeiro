1e50b1badeaee170cfe369fd55650537eaf243fc
menascim <129637296+menascim@users.noreply.github.com>
2025-02-12 19:13:56 -0300

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,6 @@
+# core/apps.py
 from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.apps.CoreConfig'  # Nome do módulo do app
+    name = 'core'  # Nome do módulo (deve ser igual ao nome da pasta do app)
