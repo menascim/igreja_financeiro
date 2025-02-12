af21bbab7d1773c6bd0439dd2bd1a7fdf0dca4de
menascim <129637296+menascim@users.noreply.github.com>
2025-02-12 20:35:50 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -7,6 +8,17 @@ from .forms import LoginForm, ContributionForm
 import pandas as pd
 from twilio.rest import Client
 import os
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Redireciona para a página de login após o cadastro
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
