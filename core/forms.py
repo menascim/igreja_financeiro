ac79c790d0d8a3399cdcc2821fde714a3f655e3c
menascim <129637296+menascim@users.noreply.github.com>
2025-02-12 20:34:41 -0300

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -8,6 +8,10 @@ class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    phone = forms.CharField(max_length=20, required=True)
+
 # Formulário de contribuição
 class ContributionForm(forms.ModelForm):
     class Meta:
@@ -16,4 +20,8 @@ class ContributionForm(forms.ModelForm):
         widgets = {
             'amount': forms.NumberInput(attrs={'class': 'form-control'}),
             'payment_method': forms.Select(attrs={'class': 'form-control'}),
+      
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'phone', 'password1', 'password2']   
         }
