230dcb1ffae7c1b7832e13500d779d9b17429505
menascim <129637296+menascim@users.noreply.github.com>
2025-02-12 20:10:35 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from core import views
+from . import views
 
 urlpatterns = [
+    path('', views.profile, name='profile'),  # Rota raiz
     path('login/', views.login_view, name='login'),
-    path('profile/', views.profile, name='profile'),
     path('export/', views.export_contributions, name='export'),
 ]
