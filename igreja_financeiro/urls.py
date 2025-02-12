088d7891708ed113b4d71b4b67e91c0d0a48f2b6
menascim <129637296+menascim@users.noreply.github.com>
2025-02-12 17:18:05 -0300

    Delete igreja_financeiro/urls.py

--- igreja_financeiro/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from core import views
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('profile/', views.profile, name='profile'),
-    path('export/', views.export_contributions, name='export'),
-]
