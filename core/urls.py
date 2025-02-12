abdb8dfc5a3b458bebe39efd156fcd14d0420ee8
menascim <129637296+menascim@users.noreply.github.com>
2025-02-12 20:41:18 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from . import views import register 
+from .views import register  # Importe a nova view
 
 urlpatterns = [
-    path('', views.profile, name='profile'),  # Rota raiz
-    path('login/', views.login_view, name='login'),
-    path('export/', views.export_contributions, name='export'),
+    # ... outras URLs ...
     path('cadastro/', register, name='cadastro'),
 ]
