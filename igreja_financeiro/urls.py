0402a2f5e1685b7c59ed50cc70f2a375995c89e8
menascim <129637296+menascim@users.noreply.github.com>
2025-02-12 19:28:49 -0300

    Update urls.py

--- igreja_financeiro/urls.py
+++ igreja_financeiro/urls.py
@@ -1,8 +1,9 @@
+# core/urls.py
 from django.urls import path
-from core import views
+from . import views
 
 urlpatterns = [
+    path('', views.profile, name='profile'),
     path('login/', views.login_view, name='login'),
-    path('profile/', views.profile, name='profile'),
     path('export/', views.export_contributions, name='export'),
 ]
