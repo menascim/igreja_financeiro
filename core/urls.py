9cb324ce3bffaedef60862e94d7edd1dd3f3ea9e
menascim <129637296+menascim@users.noreply.github.com>
2025-02-13 19:57:54 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import ProfileView
-from .views import Profile, export_contributions
+
 
 urlpatterns = [
     path('perfil/', views.profile, name='profile'),
