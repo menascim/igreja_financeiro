6ee1f51fa58ecdea505b6a96e15727e191b28ad2
menascim <129637296+menascim@users.noreply.github.com>
2025-02-13 18:24:00 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,13 @@
+# core/admin.py (adicione este arquivo)
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Informações Pessoais', {'fields': ('phone', 'email')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
