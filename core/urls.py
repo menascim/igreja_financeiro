bd78e789a06553a3f828a6449f63a2edc689e3d4
menascim <129637296+menascim@users.noreply.github.com>
2025-02-13 16:39:20 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.profile, name='profile'),  # Página inicial (após login)
-    path('login/', views.login_view, name='login'),  # Login personalizado (se necessário)
     path('cadastro/', views.register, name='cadastro'),  # Cadastro de usuários
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
