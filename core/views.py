97c74fb46cfa632a0e0f253945bd4c17fd8d2772
menascim <129637296+menascim@users.noreply.github.com>
2025-02-13 18:47:22 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -14,6 +14,7 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            login(request, user)
             return redirect('login')
     else:
         form = RegistrationForm()
