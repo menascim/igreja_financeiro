e843bac559a50d58e6560b886b155ed2dcc28096
menascim <129637296+menascim@users.noreply.github.com>
2025-02-13 18:42:49 -0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # Inclui as URLs do app "core"
     path('accounts/', include('django.contrib.auth.urls')),  # URLs padrão de autenticação (login/logout)
+    path(settings.ADMIN_URL, admin.site.urls),  # Usa o caminho configurado
 ]
