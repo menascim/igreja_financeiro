483a99b7ddf565023e71b2658bfa25a09af78028
menascim <129637296+menascim@users.noreply.github.com>
2025-02-13 09:05:31 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile'),
-    path('login/', views.login_view, name='login'),
-    path('cadastro/', views.register, name='cadastro'),
+    path('', views.profile, name='profile'),  # Página inicial (após login)
+    path('login/', views.login_view, name='login'),  # Login personalizado (se necessário)
+    path('cadastro/', views.register, name='cadastro'),  # Cadastro de usuários
 ]
