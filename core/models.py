81f504b7247d689d973cb5309cba923ac6207eff
menascim <129637296+menascim@users.noreply.github.com>
2025-02-13 16:41:56 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -2,9 +2,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    phone = models.CharField(max_length=20, unique=True)
-    role = models.CharField(max_length=50, blank=True)
-
+    phone = models.CharField(
+        max_length=20, 
+        unique=True,
+        error_messages={'unique': 'Este telefone já está registrado!'}
+    )
 class Contribution(models.Model):
     PAYMENT_METHODS = [
         ('PIX', 'PIX'),
