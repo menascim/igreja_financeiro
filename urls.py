617194b5e672ee597cf4268f6b95bb296a90e0b8
menascim <129637296+menascim@users.noreply.github.com>
2025-02-13 09:48:11 -0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # Inclui as URLs do app "core"
-    path('accounts/', include('django.contrib.auth.urls')),  # URLs de autenticação padrão (login/logout)
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('core.urls')),
 ]
