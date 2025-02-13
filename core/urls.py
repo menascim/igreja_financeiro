2f7f2677751a4aaee7ddc0f2f5faa5ef163a8537
menascim <129637296+menascim@users.noreply.github.com>
2025-02-13 18:53:21 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('exportar/', views.export_contributions, name='export'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
 ]
 
 
