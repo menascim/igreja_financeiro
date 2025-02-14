d8f6bc312fbe1f926fb5cf621cd7bac39ed3181b
menascim <129637296+menascim@users.noreply.github.com>
2025-02-14 12:22:39 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from .import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import ProfileView
 from .views import ProfileView, add_contribution
