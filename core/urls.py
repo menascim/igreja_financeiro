f5df8625c32a0650b419de409a1420f8fa512ace
menascim <129637296+menascim@users.noreply.github.com>
2025-02-14 21:33:49 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
-from .import views
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import ProfileView
-from .views import ProfileView, add_contribution
-from .views import UserAutocomplete
-
+from django.contrib.auth.views import LogoutView
+from . import views
+from .views import (
+    ProfileView,
+    add_contribution,
+    UserAutocomplete,
+    export_contributions
+)
 
 urlpatterns = [
-   path('', ProfileView.as_view(), name='home'),
+    path('', ProfileView.as_view(), name='home'),
     path('cadastro/', views.register, name='cadastro'),
-    path('login/', views.login_view, name='login'),  # Usando a view personalizada
+    path('login/', views.login_view, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('contributions/add/', views.add_contribution, name='add_contribution'),
     path('exportar/', views.export_contributions, name='export'),
-   path('user-autocomplete/', UserAutocomplete.as_view(), name='user-autocomplete'),
+    path('user-autocomplete/', UserAutocomplete.as_view(), name='user-autocomplete'),
 ]
-
-
