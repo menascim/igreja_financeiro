b0972ac5227902e5a34ea214b66d499a6fc6b814
menascim <129637296+menascim@users.noreply.github.com>
2025-02-14 21:45:11 -0300

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,26 +1,17 @@
 # core/forms.py
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.forms import UserCreationForm
-from .models import Contribution
-from .models import CustomUser
-from django.core.validators import EmailValidator  # Adicione esta linha
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.core.validators import EmailValidator
+from .models import Contribution, CustomUser
 from dal import autocomplete
 
-
-# Formulário de login
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(
-        validators=[EmailValidator(message="E-mail inválido!")],  # Uso correto
-        widget=forms.EmailInput(attrs={'class': 'form-control'})
-    )
-    
-    email = forms.EmailField(
-        validators=[EmailValidator(message="Insira um e-mail válido!")],  # Corrigido
+        validators=[EmailValidator(message="Insira um e-mail válido!")],
         widget=forms.EmailInput(attrs={'class': 'form-control'})
     )
 
@@ -37,29 +28,29 @@ class ContributionForm(forms.ModelForm):
         widget=autocomplete.ModelSelect2(
             url='user-autocomplete',
             attrs={'data-html': True}
-        )
+        ),
+        required=False
     )
+
+    metodo = forms.ChoiceField(
+        label="Método de Pagamento",
+        choices=Contribution.PAYMENT_METHODS,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    class Meta:
+        model = Contribution
+        fields = ['user', 'valor', 'metodo', 'data']
+        widgets = {
+            'valor': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
+            'data': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'})
+        }
 
     def __init__(self, *args, **kwargs):
         is_admin = kwargs.pop('is_admin', False)
         super().__init__(*args, **kwargs)
 
         if not is_admin:
-            del self.fields['user']  # Esconde o campo para não-admins
+            del self.fields['user']
         else:
             self.fields['user'].queryset = CustomUser.objects.filter(is_staff=False)
-            
-        widgets = {
-            'metodo': forms.Select(attrs={'class': 'form-control'}),
-            'valor': forms.NumberInput(attrs={
-                'class': 'form-control',
-                'step': '0.01'
-            })
-        }
-
-    # Opcional: Para personalizar o label ou traduzir
-    metodo = forms.ChoiceField(
-        label="Método de Pagamento",
-        choices=Contribution.PAYMENT_METHODS,
-        widget=forms.Select(attrs={'class': 'form-control'})
-    )
